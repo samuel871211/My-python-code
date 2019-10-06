42d45db105eea19d46e08ff429e7eb41ee341d11
samuel871211 <55658154+samuel871211@users.noreply.github.com>
2019-10-06 23:34:18 +0800

    Update min stack.py

--- week3/min stack.py
+++ week3/min stack.py
@@ -8,9 +8,9 @@ class MinStack(object):
         self.stack.append((x, min(x, self.getMin())))
         
 
-    def pop(self): #定義pop函數
+    def pop(self): #定義pop函數，用來取得stack最上面的值
         if len(self.stack)==0: return None #如果self.stack的長度=0，就return none
-        return self.stack.pop()[0] #否則就return
+        return self.stack.pop()[0] #return self.stack最上面的值 [0]代表資料的第0個位置
         
 
     def top(self):
