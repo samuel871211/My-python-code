28babc81078eb8c872e064f239fc8c1c9df52070
samuel871211 <55658154+samuel871211@users.noreply.github.com>
2019-10-16 23:31:44 +0800

    Create insertion sort using list(Inplace version)(mycode).py

---
+++ week4/insertion sort using list(Inplace version)(mycode).py
@@ -0,0 +1,11 @@
+def insertion_sort(list):
+    for index in range(1,len(list)): #最左邊的數字沒得比較，所以從第1個數字開始比
+        value = list[index] #把第index位的數字取出來等等需要來比較
+        i = index - 1 #要跟左邊的那個數字來比較
+        while i >= 0: #一直跟左邊的比，直到到最左邊的數字
+            if value < list[i]: #右邊比左邊小
+                list[i+1] = list[i] #把左邊的位置丟到右邊
+                list[i] = value #把左邊的值丟到右邊
+                i = i - 1 #一直跟左邊的比，直到到最左邊的數字
+            else:
+                break
