fe15a79d6cc3cb5c0003009350e4719117dc71f2
samuel871211 <55658154+samuel871211@users.noreply.github.com>
2019-11-01 11:32:52 +0800

    Update BinaryTree.py

--- HW3/BinaryTree.py
+++ HW3/BinaryTree.py
@@ -1,1 +1,25 @@
-class Node(self,element,parent,left,right): 
+class Node:
+    def __init__ (self,val):
+        self.root = None
+        self.size = 0
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def AddAtRoot
+    "add a node of value at the root position"
+    
+    def AddAtLeft
+    "add a node of value at the left position"
+    
+    def AddAtRight
+    "add a node of value at the right position"
+    
+    def replace
+    ""
+    
+    def DeleteAtIndex
+    "delete a node of value at the index-th position"
+    
+    def depth
+    "find the depth of the node"
