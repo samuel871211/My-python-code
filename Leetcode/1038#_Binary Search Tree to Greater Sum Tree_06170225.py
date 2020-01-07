584b06c3f6915689abb16cc4f9b86dfabf5ce256
samuel871211 <55658154+samuel871211@users.noreply.github.com>
2020-01-07 13:18:51 +0800

    Create 1038#_Binary Search Tree to Greater Sum Tree_06170225.py

---
+++ Leetcode/1038#_Binary Search Tree to Greater Sum Tree_06170225.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root == None or (root.left == None and root.right == None):
+            return root
+        else:
+            self.sum = 0
+            self.helper(root)
+            return root
+    
+    def helper(self,root):
+        if root.right:
+            self.helper(root.right)
+        
+        self.sum += root.val
+        root.val = self.sum
+        
+        if root.left:
+            self.helper(root.left)
