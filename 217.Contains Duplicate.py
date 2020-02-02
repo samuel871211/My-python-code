4bfd8ece8a006c3c1000122d53f71c6e99479f3f
samuel871211 <55658154+samuel871211@users.noreply.github.com>
2020-02-02 16:45:15 +0800

    Delete 217.Contains Duplicate.py

--- 217.Contains Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
-        for i in range(len(nums)):
-            cur = len(s)
-            s.add(nums[i])
-            if len(s) == cur:
-                return True
-        return False
